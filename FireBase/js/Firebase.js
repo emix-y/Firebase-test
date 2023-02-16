8bc3bbe5f88a89ac0ee376049669391067028e38
emily clark <93390989+emix-y@users.noreply.github.com>
2023-02-16 12:53:07 +0000

    Add files via upload

---
+++ FireBase/js/Firebase.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyAf5BVfSTJH-d76d0sLwicdDHsNCTOp0Ik",
+  authDomain: "experimental-media-lab-test.firebaseapp.com",
+  projectId: "experimental-media-lab-test",
+  storageBucket: "experimental-media-lab-test.appspot.com",
+  messagingSenderId: "358562770854",
+  appId: "1:358562770854:web:f85cfe9ca516e45a0e2967",
+});
+
+var db = firebase.firestore()
