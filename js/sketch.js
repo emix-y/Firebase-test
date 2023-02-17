a0e7dbf2cd05d293c57e2d09f3e5a168d1522d10
emily clark <93390989+emix-y@users.noreply.github.com>
2023-02-17 11:49:27 +0000

    Add files via upload
    final commit, firebase data and circle generation

---
+++ js/sketch.js
@@ -0,0 +1,15 @@
+function setup(){
+var canvas = createCanvas(window.innerWidth/2, window.innerHeight)
+canvas.parent('sketchContainer');
+
+db.collection('circles').get().then((querySnapshot)=>{
+   querySnapshot.forEach((doc) =>{
+       fill(doc.data().color);
+       ellipse(doc.data().x, doc.data().y, doc.data().size)
+   })
+})
+}
+
+function draw(){
+
+}
